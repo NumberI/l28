3587ce163c2c381182edeaa62e195af41f4c388b
NumberI <aksenov@AKSENOV.oscaryard.local>
2015-10-20 16:55:01 +0300

    add comments

--- app.rb
+++ app.rb
@@ -34,3 +34,10 @@ post '/new' do
 	@db.execute "INSERT INTO Posts ( created_date , content ) VALUES ( datetime(), ?)", [content]
 	redirect to "/"
 end
+
+get "/details/:post_id" do
+	post_id = params[:post_id]
+	results = @db.execute "SELECT * FROM Posts where id = ?",[post_id]
+	@row = results[0]
+	erb :details
+end
