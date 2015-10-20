c8046fc502d95d8872ee17b2b88abfa3a87cee41
NumberI <aksenov@AKSENOV.oscaryard.local>
2015-10-20 16:18:42 +0300

    add index

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
 get '/new' do
 	erb :new		
@@ -28,6 +28,8 @@ post '/new' do
 	content = params[:text]
 	if content.length <= 0
 		@error = "Введите текст"
+		return erb :new
 	end
+	@db.execute "INSERT INTO Posts ( created_date , content ) VALUES ( datetime(), ?)", [content]
 	erb "#{content}"	
 end
